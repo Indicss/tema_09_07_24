5f604a13e7dda50df076ad02bb5954e39e62d6ff
Jalba Oleg <jalbaoleg00@gmail.com>
2024-07-09 00:55:34 +0300

    tema

---
+++ tema_09_07_24/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace EmployeeManagement
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public decimal Salary { get; set; }
+
+        public Employee(string name, int id, decimal salary)
+        {
+            Name = name;
+            ID = id;
+            Salary = salary;
+        }
+
+        public virtual decimal CalculateAnnualSalary()
+        {
+            return Salary;
+        }
+    }
+
+    public class FullTimeEmployee : Employee
+    {
+        public decimal Bonus { get; set; }
+
+        public FullTimeEmployee(string name, int id, decimal salary, decimal bonus)
+            : base(name, id, salary)
+        {
+            Bonus = bonus;
+        }
+
+        public override decimal CalculateAnnualSalary()
+        {
+            return Salary * 12 + Bonus;
+        }
+    }
+
+    public class PartTimeEmployee : Employee
+    {
+        public decimal HourlyRate { get; set; }
+        public int HoursPerWeek { get; set; }
+
+        public PartTimeEmployee(string name, int id, decimal hourlyRate, int hoursPerWeek)
+            : base(name, id, hourlyRate * hoursPerWeek * 4)
+        {
+            HourlyRate = hourlyRate;
+            HoursPerWeek = hoursPerWeek;
+        }
+
+        public override decimal CalculateAnnualSalary()
+        {
+            return HourlyRate * HoursPerWeek * 52;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FullTimeEmployee fullTimeEmp = new FullTimeEmployee("Oleg", 1, 3000m, 5000m);
+            Console.WriteLine($"Full-Time: {fullTimeEmp.Name}, salariu anual: {fullTimeEmp.CalculateAnnualSalary()}");
+
+            PartTimeEmployee partTimeEmp = new PartTimeEmployee("Igor", 2, 20m, 20);
+            Console.WriteLine($"Part-Time: {partTimeEmp.Name}, salariu anual: {partTimeEmp.CalculateAnnualSalary()}");
+        }
+    }
+}
---
+++ tema_09_07_24/ProgramBase.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibraryManagement
+{
+    public interface IBorrowable
+    {
+        void Borrow();
+        void Return();
+        bool IsBorrowed { get; }
+    }
+
+    public abstract class LibraryItem : IBorrowable
+    {
+        public string Titlu { get; set; }
+        public string Autor { get; set; }
+        public int AnPublicare { get; set; }
+        public bool IsBorrowed { get; private set; }
+
+        public LibraryItem(string titlu, string autor, int anPublicare)
+        {
+            Titlu = titlu;
+            Autor = autor;
+            AnPublicare = anPublicare;
+            IsBorrowed = false;
+        }
+
+        public void Borrow()
+        {
+            if (!IsBorrowed)
+            {
+                IsBorrowed = true;
+                Console.WriteLine($"{Titlu} a fost imprumutat.");
+            }
+            else
+            {
+                Console.WriteLine($"{Titlu} este deja imprumutat.");
+            }
+        }
+
+        public void Return()
+        {
+            if (IsBorrowed)
+            {
+                IsBorrowed = false;
+                Console.WriteLine($"{Titlu} a fost returnat.");
+            }
+            else
+            {
+                Console.WriteLine($"{Titlu} nu a fost imprumutat.");
+            }
+        }
+    }
+
+    public class Book : LibraryItem
+    {
+        public Book(string titlu, string autor, int anPublicare)
+            : base(titlu, autor, anPublicare)
+        {
+        }
+    }
+
+    public class Magazine : LibraryItem
+    {
+        public Magazine(string titlu, string autor, int anPublicare)
+            : base(titlu, autor, anPublicare)
+        {
+        }
+    }
+
+    public class DVD : LibraryItem
+    {
+        public DVD(string titlu, string autor, int anPublicare)
+            : base(titlu, autor, anPublicare)
+        {
+        }
+    }
+
+    public class Library
+    {
+        private List<LibraryItem> items;
+
+        public Library()
+        {
+            items = new List<LibraryItem>();
+        }
+
+        public void AddItem(LibraryItem item)
+        {
+            items.Add(item);
+            Console.WriteLine($"{item.Titlu} a fost adaugat în biblioteca.");
+        }
+
+        public void BorrowItem(string titlu)
+        {
+            LibraryItem item = items.Find(i => i.Titlu == titlu);
+            if (item != null)
+            {
+                item.Borrow();
+            }
+            else
+            {
+                Console.WriteLine($"Itemul {titlu} nu a fost gasit in biblioteca.");
+            }
+        }
+        public void ReturnItem(string titlu)
+        {
+            LibraryItem item = items.Find(i => i.Titlu == titlu);
+            if (item != null)
+            {
+                item.Return();
+            }
+            else
+            {
+                Console.WriteLine($"Itemul {titlu} nu a fost gasit în biblioteca.");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library();
+
+            library.AddItem(new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925));
+            library.AddItem(new Magazine("National Geographic", "Diverse", 2021));
+            library.AddItem(new DVD("Inception", "Christopher Nolan", 2010));
+
+            library.BorrowItem("The Great Gatsby");
+            library.ReturnItem("The Great Gatsby");
+            library.BorrowItem("National Geographic");
+            library.ReturnItem("Inception");
+        }
+    }
+}
